dc4641518be4e0c35fe96fdb024ed3f8298beba3
Yash0259 <bhamreyash2002.in>
2025-03-30 13:45:39 +0530

    Update Backend

--- Backend/app/controllers/user_controller.py
+++ Backend/app/controllers/user_controller.py
@@ -2,14 +2,12 @@ from app.models.user import users_collection
 from app.schemas.user_schema import UserSchema
 from bson import ObjectId
 
-# Create a new user
-def create_user(user: UserSchema):
-    user_dict = user.dict()
-    user_dict["_id"] = str(ObjectId())  # Convert MongoDB _id to string
+def create_user(user : UserSchema):
+    user_dist : user.dict()
+    user_dict["id"] = str(ObjectId()) // convert mongoDB _id to string
     users_collection.insert_one(user_dict)
-    return {"message": "User created successfully", "user_id": user_dict["_id"]}
+    return{ "message":"User created Successfully","user_id":user_dict["_id"]}
 
-# Get all users
 def get_users():
-    users = users_collection.find({}, {"_id": 1, "name": 1, "email": 1})  # Only return name & email
-    return [{"id": str(user["_id"]), "name": user["name"], "email": user["email"]} for user in users]
+    users = users_collection.find({},{"_id":1,"name":1,"email":1})
+    return[{"id":str(user["_id"]),"name":user["name"],"email":user["email"]} for user in users]
